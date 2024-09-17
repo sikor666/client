abe985cea6cd87b07cc257a61991356d404273d5
Jacques Mayol <jacques@ocean.sea>
2024-09-17 20:09:09 +0200

    Print number of supported threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <thread>
 
 int main(int argc, char **argv)
 {
+    unsigned int n = std::thread::hardware_concurrency();
+    std::cout << n << " concurrent threads are supported.\n";
+
     return 0;
 }
