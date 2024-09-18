42680f0a0ca7498a7139ca6574bdfb6aef9fae97
Jacques Mayol <jacques@ocean.sea>
2024-09-18 15:29:47 +0200

    Add progress and profiler to server

--- server.cpp
+++ server.cpp
@@ -1,7 +1,12 @@
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <string>
- 
+
+#include <experimental/random>
+
+constexpr auto MAX_WORDS = 32000000ul;
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -20,15 +25,26 @@ int main(int argc, char **argv)
     if (not ostrm.good())
         throw std::runtime_error("Error has occurred or I/O operations aren't available");
 
-    ostrm << "abc ";
-    ostrm << "tomek ";
-    ostrm << "jest ";
-    ostrm << "pan";
+    const auto start = std::chrono::steady_clock::now();
 
-    for (char i = 'a'; i <= 'z'; i++)
+    for (size_t i = 0; i < MAX_WORDS; i++)
     {
-        std::cout << i << " ";
+        std::cout << "\r" << (static_cast<double>(i) / static_cast<double>(MAX_WORDS)) * 100.0 << " %";
+
+        size_t len = std::experimental::randint(1, 16);
+
+        while (len-- > 0)
+        {
+            ostrm << static_cast<char>(std::experimental::randint(int('a'), int('z')));
+        }
+
+        ostrm << " ";
     }
+
+    const auto stop = std::chrono::steady_clock::now();
+    const auto time = std::chrono::duration_cast<std::chrono::seconds>(stop - start).count();
+
+    std::cout << "\n" << "time: " << time << " s\n";
 
     return 0;
 }
