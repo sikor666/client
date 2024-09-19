93278e61ba11dc44a4293be067d6d25180bece0b
Jacques Mayol <jacques@ocean.sea>
2024-09-19 18:21:41 +0200

    Add debug

--- WordsCounter.cpp
+++ WordsCounter.cpp
@@ -85,3 +85,7 @@ WordsCounter::WordsCounter(FILE * stream, long origin, long offset)
 
     std::cout << "[bytes: " << bytes << "]\n";
 }
+
+// text.txt
+// 6  6  6  _  H  a  c  k  e  r  _  | T  o  m  e  k  _  6  6  6  _  | H  a  c  k  e  r  _  T  o  m  e  k  \n
+// 00 01 02 03 04 05 06 07 08 09 10 | 11 12 13 14 15 16 17 18 19 20 | 21 22 23 24 25 26 27 28 29 30 31 32 33
